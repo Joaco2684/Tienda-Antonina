9b9314b5c49063e701ec230c1abf61d24948470c
Joaco2684 <Joaquindilo2684@gmail.com>
2022-01-31 06:52:26 -0300

    first commit

---
+++ src/models/productModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const productSchemma = new mongoose.Schema({
+    product_id: {
+        type: String,
+        unique: true,
+        trim: true,
+        required:true
+    },
+    title: {
+        type: String,
+        trim: true,
+        required:true
+    },
+    price: {
+        type: Number,
+        trim: true,
+        required:true
+    },
+    description: {
+        type: String,
+        required:true
+    },
+    content: {
+        type: String,
+        required:true
+    },
+    images: {
+        type: Object,
+        required:true
+    },
+    category: {
+        type: String,
+        required:true
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+}, { timestamp: true });
+
+module.exports = mongoose.model('Products', productSchemma)
